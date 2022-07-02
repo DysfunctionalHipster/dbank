29fe1accd920403b7d9c04dc76c73a47fbdd5f7c
Jamie <jamiecnisbet@gmail.com>
2022-07-02 07:22:21 +0200

    comments added

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -2,15 +2,17 @@ import Debug "mo:base/Debug";
 import Time "mo:base/Time";
 import Float "mo:base/Float";
 
-actor DBank {
-  stable var currentValue: Float = 300;
+// -!- Cannister 1 -!-
 
+actor DBank { 
+
+  stable var currentValue: Float = 300;
   stable var startTime = Time.now();
 
   public func topUp(amount: Float) {
     currentValue += amount;
     // Debug.print(debug_show(currentValue));
-  };
+  }; // -this- Adds user amount to balance
 
   public func withdraw(amount: Float) {
     let balance: Float = currentValue - amount;
@@ -20,11 +22,11 @@ actor DBank {
     } else {
       Debug.print("Not enough money buddy")
     }
-  };
+  }; // -this- Removes user amount from balance
 
   public query func checkBalance(): async Float {
     return currentValue;
-  };
+  }; // -this- Checks balance
 
   public func compound() {
     let currentTime = Time.now();
@@ -32,5 +34,5 @@ actor DBank {
     let timeElapsedMinutes = timeElapsedNaSecs / 60000000000;
     currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedMinutes));
     startTime := currentTime;
-  }
+  }; // -this- Compounds the interest of the balance
 }
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,40 +1,36 @@
 import { UpdateCallRejectedError } from "@dfinity/agent";
 import { dbank } from "../../declarations/dbank";
 
-window.addEventListener("load", async function() {
-    console.log('Finished loading');
-    update();
-});
+window.addEventListener("load", update()); // -ok- Checks the account balance
 
 document.querySelector('form').addEventListener('submit', async function(event) {
     event.preventDefault();
-    console.log('Submitted');
 
     const button = event.target.querySelector("#submit-btn");
 
-    const inputAmount = parseFloat(document.getElementById('input-amount').value);
-    const outputAmount = parseFloat(document.getElementById('withdraw-amount').value);
+    const inputAmount = parseFloat(document.getElementById('input-amount').value); // -this- Takes the user's input and transforms it into a float
+    const outputAmount = parseFloat(document.getElementById('withdraw-amount').value); // -this- Takes the user's input and transforms it into a float
 
-    button.setAttribute('disabled', true);
+    button.setAttribute('disabled', true); // -this- disables the submit button
 
     if (document.getElementById('input-amount').value.length != 0) {
     await dbank.topUp(inputAmount);
-    };
+    }; // -this- Checks if the "add to account" field is empty
  
     if (document.getElementById('withdraw-amount').value.length != 0) {
     await dbank.withdraw(outputAmount);
-    };
+    }; // -this- Checks if the "remove from account" field is empty
 
-    await dbank.compound();
+    await dbank.compound(); // -this- Compounds the current balance
 
     update();
 
-    document.getElementById('input-amount').value = "";
-    document.getElementById('withdraw-amount').value = "";
-    button.removeAttribute('disabled');
+    document.getElementById('input-amount').value = ""; // -ok- Resets the value to empty after function
+    document.getElementById('withdraw-amount').value = ""; // -ok- Resets the value to empty after function
+    button.removeAttribute('disabled'); // -this- Re-ables the submit button after the functions are over
 });
 
 async function update() {
     const currentAmount = await dbank.checkBalance();
     document.getElementById('value').innerText = Math.round(currentAmount * 100) / 100;
-}
+}; // -this- Checks the current balance
